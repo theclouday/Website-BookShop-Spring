36523670fe11f48d3e67fbadd47741111302b413
theclouday <theclouday@example.com>
2024-03-26 20:46:10 +0100

    prepare request for future functionality admin page

---
+++ src/main/java/com/BookShop/WepAppBookShop/Page/Service/PageAdminPanel.java
@@ -0,0 +1,13 @@
+package com.BookShop.WepAppBookShop.Page.Service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class PageAdminPanel {
+    private String adminIP = "123.123.1.2"; // example reserved IP adress
+
+
+//    public ipChecking(adminIP) {
+//        return;
+//    }
+}
